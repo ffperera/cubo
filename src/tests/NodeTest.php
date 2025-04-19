121ba8aaab7ffe62813f41f02cc246675ca8e139
Felipe Fernandez Perera <felipe@epsilon-eridani.com>
2025-04-19 11:54:00 +0200

    Add NodeTest and enhance RenderTest with setup and teardown methods

---
+++ src/tests/NodeTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Action1 extends \FFPerera\Cubo\Action
+{
+    public function run(\FFPerera\Cubo\Controller $controller): void {}
+}
+
+class Action2 extends \FFPerera\Cubo\Action
+{
+    public function run(\FFPerera\Cubo\Controller $controller): void {}
+}
+
+
+class NodeTest extends TestCase
+{
+    protected \FFPerera\Cubo\Action $actionObjectOne;
+    protected \FFPerera\Cubo\Action $actionObjectTwo;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->actionObjectOne = new Action1();
+        $this->actionObjectTwo = new Action2();
+    }
+
+
+    public function testCreateNodeWithAnAction()
+    {
+        $node = new \FFPerera\Cubo\Node($this->actionObjectOne);
+
+        $this->assertSame($this->actionObjectOne, $node->getAction());
+    }
+
+    public function testSettingNextNode()
+    {
+        $action1 = $this->actionObjectOne;
+        $action2 = $this->actionObjectTwo;
+
+        $node1 = new \FFPerera\Cubo\Node($action1);
+        $node2 = new \FFPerera\Cubo\Node($action2);
+
+        $node1->setNext($node2);
+
+        $this->assertSame($node2, $node1->getNext());
+    }
+
+    public function testSetAction()
+    {
+        $node = new \FFPerera\Cubo\Node($this->actionObjectOne);
+
+        $node->setAction($this->actionObjectOne);
+        $this->assertSame($this->actionObjectOne, $node->getAction());
+    }
+}
--- src/tests/RenderTest.php
+++ src/tests/RenderTest.php
@@ -4,18 +4,57 @@ use PHPUnit\Framework\TestCase;
 
 class RenderTest extends TestCase
 {
-    public function testMethods()
+
+    private static string $templateFile;
+    private static string $templateContent = 'Template Content <?php $this->block("block"); ?>';
+    private static string $blockFile;
+    private static string $blockContent = 'Block Content';
+
+    private static string $rootDirectory;
+
+    public static function setUpBeforeClass(): void
     {
-        $render = new \FFPerera\Cubo\Render(new \FFPerera\Cubo\View());
+        self::$rootDirectory = sys_get_temp_dir() . '/';
 
-        // test default  src directory
+        self::$templateFile = 'test_template.php';
+        file_put_contents(self::$rootDirectory . self::$templateFile, self::$templateContent);
+
+        self::$blockFile = 'test_block.php';
+        file_put_contents(self::$rootDirectory . self::$blockFile, self::$blockContent);
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        if (file_exists(self::$rootDirectory . self::$templateFile)) {
+            unlink(self::$rootDirectory . self::$templateFile);
+        }
+        if (file_exists(self::$rootDirectory . self::$blockFile)) {
+            unlink(self::$rootDirectory . self::$blockFile);
+        }
+    }
+
+
+    public function testCreateRenderWithoutRootDirectory()
+    {
+        $view = new \FFPerera\Cubo\View();
+        $render = new \FFPerera\Cubo\Render($view);
+
+        // Check if the root directory is set to the document root
         $this->assertEquals($_SERVER['DOCUMENT_ROOT'], $render->getRootDirectory());
+    }
 
-        // test custom src directory
+    public function testCreateRenderWithRootDirectory()
+    {
         $customDirectory = '/path/to/custom/directory';
-        $render = new \FFPerera\Cubo\Render(new \FFPerera\Cubo\View(), $customDirectory);
-        $this->assertEquals($customDirectory, $render->getRootDirectory());
+        $view = new \FFPerera\Cubo\View();
+        $render = new \FFPerera\Cubo\Render($view, $customDirectory);
 
+        // Check if the root directory is set to the custom directory
+        $this->assertEquals($customDirectory, $render->getRootDirectory());
+    }
+
+    public function testSetAndGetAViewObject()
+    {
         // test set and get view
         $view = new \FFPerera\Cubo\View();
         $render = new \FFPerera\Cubo\Render($view);
@@ -25,33 +64,33 @@ class RenderTest extends TestCase
     public function testSend()
     {
         $view = new \FFPerera\Cubo\View();
-        $view->setLayout('layout.php');
+        $view->setLayout(self::$templateFile);
 
-        // mock the insert method
-        $render = $this->getMockBuilder(\FFPerera\Cubo\Render::class)
-            ->setConstructorArgs([$view])
-            ->onlyMethods(['insert'])
-            ->getMock();
-        $render->expects($this->once())
-            ->method('insert')
-            ->with($this->equalTo('layout.php'));
+        // Create a Render instance with the directory containing the template
+        $render = new \FFPerera\Cubo\Render($view, self::$rootDirectory);
+        // Capture the output of the send method
+        ob_start();
         $render->send();
+        $output = ob_get_clean();
+
+        // Assert that the output matches the template content
+        $this->assertEquals('Template Content ', $output);
     }
 
     public function testBlock()
     {
         $view = new \FFPerera\Cubo\View();
-        $view->setLayout('layout.php');
-        $view->setTemplate('header', 'header.php');
+        $view->setLayout(self::$templateFile);
+        $view->setTemplate('block', self::$blockFile);
 
-        // mock the insert method
-        $render = $this->getMockBuilder(\FFPerera\Cubo\Render::class)
-            ->setConstructorArgs([$view])
-            ->onlyMethods(['insert'])
-            ->getMock();
-        $render->expects($this->once())
-            ->method('insert')
-            ->with($this->equalTo('header.php'));
-        $render->block('header');
+        // Create a Render instance with the directory containing the template
+        $render = new \FFPerera\Cubo\Render($view, self::$rootDirectory);
+        // Capture the output of the send method
+        ob_start();
+        $render->send();
+        $output = ob_get_clean();
+
+        // Assert that the output matches the template content
+        $this->assertEquals('Template Content Block Content', $output);
     }
 }
